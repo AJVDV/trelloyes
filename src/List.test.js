3bee7c0b0bc0302004510857df8bd8d422a9bf91
Andrew VanDerVeen <69212799+AJVDV@users.noreply.github.com>
2020-10-22 11:03:02 -0700

    Added smoke and snapshot testing to Card.js and List.js
    List.js testing is nonfunctional, will need assistance to fix

---
+++ src/List.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List';
+import Card from './Card';
+
+let cards = {Card};
+
+cards.defaultProps = {
+    store: {
+    lists: [],
+    allCards: {},
+    }
+};
+
+describe('Messages component', () => {
+    it('renders without crashing', () => {
+      const section = document.createElement('section');
+      ReactDOM.render(<List  />, section);
+      ReactDOM.unmountComponentAtNode(section);
+    });
+  
+    it('renders the UI as expected', () => {
+      const tree = renderer
+        .create(<List name="List"  />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();   
+    });
+  });
